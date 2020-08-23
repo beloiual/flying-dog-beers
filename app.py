1a2fa6541293f9510a4023721d781cd67232d91b
beloiual <beloiual@umich.edu>
2020-08-23 00:47:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dash_table import DataTable
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app=JupyterDash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 colors = {
     'background': '#fbf9c7',
